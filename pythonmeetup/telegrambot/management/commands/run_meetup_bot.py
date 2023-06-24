5b2a50b7d9d136ef92cc597af3a7857ab2053770
Dzima-G <dgukalin@gmail.com>
2023-06-24 18:09:56 +0300

    add an entry to the bot database for listeners

--- pythonmeetup/telegrambot/management/commands/run_meetup_bot.py
+++ pythonmeetup/telegrambot/management/commands/run_meetup_bot.py
@@ -2,10 +2,10 @@ from django.core.management.base import BaseCommand
 from pythonmeetup import settings
 from django.db import models
 from ...models import Listener, Question, Lecture, Event
-from environs import Env
 import datetime
 import json
 import logging
+from django.utils.timezone import localtime
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (
     Updater,
@@ -13,7 +13,9 @@ from telegram.ext import (
     CallbackQueryHandler,
     ConversationHandler,
     CallbackContext,
-    TypeHandler
+    TypeHandler,
+    Filters,
+    MessageHandler,
 )
 
 # Ведение журнала логов
@@ -24,7 +26,7 @@ logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = 'Телеграм-бот для пользователя'
+    help = 'Телеграм-бот для слушателя'
 
     def handle(self, *args, **kwargs):
         tg_token = settings.TG_LISTENER_TOKEN
@@ -32,8 +34,6 @@ class Command(BaseCommand):
         dispatcher = updater.dispatcher
 
         def start(update, _):
-            """Вызывается по команде `/start`."""
-            # Получаем пользователя, который запустил команду `/start`
             user = update.message.from_user
             logger.info("Пользователь %s начал разговор", user.first_name)
 
@@ -42,37 +42,33 @@ class Command(BaseCommand):
                 [InlineKeyboardButton("Задать вопрос по текущему докладу", callback_data='to_question')]
             ]
             reply_markup = InlineKeyboardMarkup(keyboard)
-            # Отправляем сообщение с текстом и добавленной клавиатурой `reply_markup`
             update.message.reply_text(
-                text="Мы рады Вас приветствовать в сервисе PythonMeetup \n"
-                     "Наша площадка проводит онлайн конференции", reply_markup=reply_markup
+                text=f"Мы рады Вас приветствовать {user.first_name} в сервисе PythonMeetup. \n", reply_markup=reply_markup
             )
-            # Сообщаем `ConversationHandler`, что сейчас состояние `FIRST`
             return 'FIRST'
 
         def start_over(update, _):
-            """Тот же текст и клавиатура, что и при `/start`, но не как новое сообщение"""
-            # Получаем `CallbackQuery` из обновления `update`
             query = update.callback_query
-            # На запросы обратного вызова необходимо ответить,
-            # даже если уведомление для пользователя не требуется.
-            # В противном случае у некоторых клиентов могут возникнуть проблемы.
             query.answer()
             keyboard = [
                 [InlineKeyboardButton("Посмотреть план мероприятия", callback_data='event_plan')],
                 [InlineKeyboardButton("Задать вопрос по текущему докладу", callback_data='to_question')]
             ]
             reply_markup = InlineKeyboardMarkup(keyboard)
-            # Отредактируем сообщение, вызвавшее обратный вызов.
-            # Это создает ощущение интерактивного меню.
             query.edit_message_text(
-                text="Мы рады Вас приветствовать в сервисе PythonMeetup \n"
-                     "Наша площадка проводит онлайн конференции", reply_markup=reply_markup)
+                text="Мы рады Вас приветствовать в сервисе PythonMeetup.", reply_markup=reply_markup)
             return 'FIRST'
 
         def get_schedule_events(update, _):
-            """Показ нового выбора кнопок"""
-            data = Listener.objects.all()
+            date = datetime.date.today()
+            schedule = ''
+            for i, item in enumerate(Lecture.objects.all(), 1):
+                if i == 1:
+                    str_str = f'{item.event} \n Рассписание на {date} \n'
+                    schedule = schedule + str_str
+                if item.date == date:
+                    str_str = f'{i}. {item.topic}, Докладчик: {str(item.speaker).split(" ")[0]}, Время проведения: {item.start_time} - {item.end_time} \n'
+                    schedule = schedule + str_str
             query = update.callback_query
             query.answer()
             keyboard = [
@@ -81,15 +77,10 @@ class Command(BaseCommand):
                 ]
             ]
             reply_markup = InlineKeyboardMarkup(keyboard)
-            query.edit_message_text(
-                text="Выводим таблицу или данные с расписание по мероприятию (Выводить за один день или за все мероприятие если оно длится например 2 дня)"
-                     f"{data}",
-                reply_markup=reply_markup
-            )
+            query.edit_message_text(text=f'{schedule}', reply_markup=reply_markup)
             return 'SECOND'
 
-        def get_question(update, _):
-            """Показ нового выбора кнопок"""
+        def get_question(update, context):
             query = update.callback_query
             query.answer()
             keyboard = [
@@ -101,7 +92,6 @@ class Command(BaseCommand):
             query.edit_message_text(
                 text="Напишите Ваш вопрос по текущему докладу:", reply_markup=reply_markup
             )
-            updater.dispatcher.add_handler(TypeHandler(Update, echo))
             return 'SECOND'
 
         def end(update, _):
@@ -113,12 +103,35 @@ class Command(BaseCommand):
             return ConversationHandler.END
 
         def echo(update: Update, context: CallbackContext) -> None:
-            text = json.dumps(update.to_dict(), indent=2)
+            context.bot.delete_message(chat_id=update.effective_chat.id,
+                                       message_id=update.message.message_id)
+            try:
+                create_questions(update.message.text)
+                context.bot.send_message(chat_id=update.effective_chat.id,
+                                         text=f' Отправлен Ваш вопрос:\n {update.message.text}')
+            except Exception as error:
+                context.bot.send_message(chat_id=update.effective_chat.id,
+                                         text=f'Ваш вопрос не отправлен, отсутствуют активные доклады')
+                print(f"Отсутствуют активные доклады: {str(error)}")
 
-            context.bot.send_message(chat_id=update.effective_chat.id, text=text)
-
-        # После ввода вопроса вывести - Ваш вопрос отправлен, в ближайшее время докладчик Вам ответит.
-        # И наверное переходим на главную.
+        def create_questions(question):
+            lectures = Lecture.objects.filter(
+                date=datetime.date.today(),
+                isfinished=False,
+                start_time__lte=datetime.datetime.now().time(),
+            )
+            if len(lectures) == 0:
+                raise Exception
+            for lecture in lectures:
+                listener = lecture.speaker
+                text = question
+                answered = False
+                Question.objects.create(
+                    listener=listener,
+                    lecture=lecture,
+                    text=text,
+                    answered=answered
+                )
 
         conv_handler = ConversationHandler(
             entry_points=[CommandHandler('start', start)],
@@ -134,8 +147,9 @@ class Command(BaseCommand):
             fallbacks=[CommandHandler('start', start)],
         )
 
-        # Добавляем `ConversationHandler` в диспетчер, который
-        # будет использоваться для обработки обновлений
+        echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+        dispatcher.add_handler(echo_handler)
+
         dispatcher.add_handler(conv_handler)
 
         updater.start_polling()
